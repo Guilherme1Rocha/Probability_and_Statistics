6de51ca2fc736189dfffbb5febfaaadb7036dfe1
Guilherme1Rocha <rochaguilherme035@gmail.com>
2024-05-04 15:54:32 +0100

    ex 5 corrected

--- ex_5.R
+++ ex_5.R
@@ -2,7 +2,7 @@ set.seed(2126)
 n <- 4
 r <- 150
 m <- 130
-proportions <- array(data = NA,dim = m)
+proportions <- array(data = NA,dim = r)
 sum <- 0
 counter <-0
 for (i in 1:r){
@@ -17,6 +17,5 @@ for (i in 1:r){
   counter <- 0
 }
 
-mean_proportions <- mean(proportions)
-print(dt(-0.9,4))
-print(pt(-0.9,4))
+print(round(abs((mean(proportions)-pt(-0.9,n)))*100,5))
+
