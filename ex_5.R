638f3f9b1b07c4825bc497585ef2609316ad3e68
Guilherme1Rocha <rochaguilherme035@gmail.com>
2024-06-06 14:37:05 +0100

    ex 5 corrected

--- ex_5.R
+++ ex_5.R
@@ -2,23 +2,27 @@ set.seed(2126)
 n <- 4
 r <- 150
 m <- 130
-proportions <- array(data = NA,dim = r)
-sum <- 0
-counter <-0
-for (i in 1:r){
-  for (j in 1:m){
-      Z = rnorm(n+1)
-      for (l in 1:n+1){
-        sum <- sum + Z[l]^2
-        print(l)
-      }
-      T <- (sqrt(n) * Z[1]) / sqrt(sum)
-      if (T <= -0.9) counter = counter + 1
+proportions <- numeric(r)
+counter <- 0
+
+for (i in 1:r) {
+  for (j in 1:m) {
+    Z <- rnorm(n + 1)
+    sum_Z_squared <- sum(Z[2:(n + 1)]^2)
+    T <- (sqrt(n) * Z[1]) / sqrt(sum_Z_squared)
+    if (T <= -0.9) counter <- counter + 1
   }
-  proportions[i] = counter/m
-  sum <- 0
+  proportions[i] <- counter / m
   counter <- 0
 }
 
-print(round(abs((mean(proportions)-pt(-0.9,n)))*100,5))
+# Média das proporções
+mean_proportion <- mean(proportions)
 
+# Probabilidade teórica usando distribuição t-Student com n graus de liberdade
+prob_teorica <- pt(-0.9, n)
+
+# Cálculo da diferença absoluta multiplicada por 100
+diff <- abs(mean_proportion - prob_teorica) * 100
+diff
+print(round(diff, 5))
